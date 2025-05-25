396580860b20133b6db500c10cdc7abc6440321c
Linklancien <linklancien@gmail.com>
2025-05-25 15:41:32 +0200

    rework using playint

--- war_of_attrition.v
+++ war_of_attrition.v
@@ -1,11 +1,13 @@
 module main
 
-import playint
+import linklancien.playint {Appli, Bouton}
+import os
 import gg
 import gx
 import math.vec { Vec2 }
 
 const bg_color = gg.Color{0, 0, 0, 255}
+const font_path = os.resource_abs_path('FontMono.ttf')
 
 struct App {
 mut:
@@ -20,21 +22,16 @@ mut:
 	changing_options bool
 	mouse_pos        Vec2[f32]
 
+
+	boutons_liste []Bouton
+
 	// for this project:
 	player_liste []string
 	player_trun  int
 
 	playing bool
 	// for the main menu
-	bouton_main_menu_liste []Bouton
 	// for the game
-}
-
-struct Bouton {
-mut:
-	text     string
-	pos      Vec2[f32]
-	fonction fn (mut App) @[required]
 }
 
 fn main() {
@@ -44,7 +41,7 @@ fn main() {
 		width:         100 * 6
 		height:        100 * 6
 		create_window: true
-		window_title:  '--'
+		window_title:  '-WAR OF ATTRITION-'
 		user_data:     app
 		bg_color:      bg_color
 		init_fn:       on_init
@@ -54,12 +51,12 @@ fn main() {
 		click_fn:      on_click
 		resized_fn:    on_resized
 		sample_count:  4
-		font_path:     playint.font_path
+		font_path:     font_path
 	)
 	// setup before starting
 	app.player_liste << ['RED', 'BLUE']
 
-	app.bouton_main_menu_liste << [
+	app.boutons_liste << [
 		Bouton{
 			text:     'START'
 			pos:      Vec2[f32]{
@@ -67,6 +64,8 @@ fn main() {
 				y: app.ctx.height / 2 + 30
 			}
 			fonction: game_start
+			is_visible: start_is_visible
+			is_actionnable: start_is_actionnable
 		},
 	]
 
@@ -81,16 +80,10 @@ fn on_init(mut app App) {
 }
 
 fn on_frame(mut app App) {
-	mut transparence := u8(255)
-	if app.changing_options {
-		transparence = u8(122)
-	}
 	app.ctx.begin()
-	if app.playing {
-	} else {
-		draw_main_menu(app, transparence)
-	}
-	app.opt.settings_render(app, true)
+	app.opt.settings_render(app)
+	playint.boutons_draw(mut app)
+	main_menu_render(app)
 	app.ctx.end()
 }
 
@@ -100,12 +93,8 @@ fn on_event(e &gg.Event, mut app App) {
 
 fn on_click(x f32, y f32, button gg.MouseButton, mut app App) {
 	app.mouse_pos = Vec2[f32]{x, y}
-	if app.changing_options {
-		playint.check_boutons_options(mut app)
-	} else if app.playing {
-	} else {
-		check_boutons_main_menu(mut app)
-	}
+	playint.check_boutons_options(mut app)
+	playint.boutons_check(mut app)
 }
 
 fn on_move(x f32, y f32, mut app App) {
@@ -114,60 +103,57 @@ fn on_move(x f32, y f32, mut app App) {
 
 fn on_resized(e &gg.Event, mut app App) {
 	size := gg.window_size()
+	old_x := app.ctx.width
+	old_y := app.ctx.height
+	new_x := size.width
+	new_y := size.height
 
-	if app.changing_options {
-	} else if app.playing {
-	} else {
-		for mut bouton in app.bouton_main_menu_liste {
-			x := f32(bouton.pos.x * size.width / app.ctx.width)
-			y := f32(bouton.pos.y * size.height / app.ctx.height)
-			bouton.pos = Vec2[f32]{
-				x: x
-				y: y
-			}
-		}
-	}
+	playint.boutons_pos_resize(mut app, old_x, old_y, new_x, new_y)
 
 	app.ctx.width = size.width
 	app.ctx.height = size.height
 }
 
 // main menu fn:
-fn draw_main_menu(app App, transparence u8) {
+fn main_menu_render(app App) {
 	// Main title
+	mut transparence := u8(255)
+	if app.changing_options{
+		transparence = 150
+	}
 	playint.text_rect_render(app.ctx, app.text_cfg, app.ctx.width / 2, app.ctx.height / 2,
-		false, 'War of Attrition', transparence)
+		true, true, 'War of Attrition', transparence)
+	app.ctx.draw_circle_filled(app.ctx.width / 2, app.ctx.height / 2,
+					5, gx.red)
+	app.ctx.draw_circle_filled(app.ctx.width / 2, app.ctx.height / 2 + 30,
+					5, gx.red)
 	draw_players_names(app, transparence)
-	draw_boutons_main_menu(app, transparence)
 }
 
 fn draw_players_names(app App, transparence u8) {
 	for player_id in 0 .. app.player_liste.len {
-		playint.text_rect_render(app.ctx, app.text_cfg, 0, player_id * 30, true, app.player_liste[player_id],
+		playint.text_rect_render(app.ctx, app.text_cfg, 0, player_id * 40, false, false, app.player_liste[player_id],
 			transparence)
 	}
 }
 
-fn check_boutons_main_menu(mut app App) {
-	for bouton in app.bouton_main_menu_liste {
-		if playint.point_is_in_cirle(bouton.pos, 20, app.mouse_pos) {
-			bouton.fonction(mut app)
-		}
-	}
-}
-
-fn draw_boutons_main_menu(app App, transparence u8) {
-	for bouton in app.bouton_main_menu_liste {
-		mut new_transparence := transparence
-		if playint.point_is_in_cirle(bouton.pos, 20, app.mouse_pos) {
-			new_transparence -= 75
-		}
-		playint.text_rect_render(app.ctx, app.text_cfg, bouton.pos.x, bouton.pos.y, false,
-			bouton.text, new_transparence)
-	}
-}
-
 // games fn:
-fn game_start(mut app App) {
-	app.playing = true
+fn game_start(mut app Appli) {
+	if mut app is App{
+		app.playing = true
+	}
+}
+
+fn start_is_visible (mut app Appli) bool{
+	if mut app is App{
+		return !app.playing
+	}
+	return false
+}
+
+fn start_is_actionnable (mut app Appli) bool{
+	if mut app is App{
+		return !app.playing && !app.changing_options
+	}
+	return false
 }
